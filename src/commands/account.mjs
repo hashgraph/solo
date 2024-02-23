26e2e8f47d2c05637a3c288e7c747cb115e87e1b
Jeromy Cannon <jeromy@swirldslabs.com>
2024-02-23 18:46:40 +0000

    fix: update the address book and perform helm upgrade to fix mirror node importer (#52)
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/commands/account.mjs
+++ src/commands/account.mjs
@@ -73,21 +73,8 @@ export class AccountCommand extends BaseCommand {
     const serviceMap = await this.accountManager.getNodeServiceMap(ctx.config.namespace)
 
     ctx.nodeClient = await this.accountManager.getNodeClient(ctx.config.namespace,
-      serviceMap, ctx.treasuryAccountId, ctx.treasuryPrivateKey)
+      serviceMap, ctx.treasuryAccountInfo.accountId, ctx.treasuryAccountInfo.privateKey)
     this.nodeClient = ctx.nodeClient // store in class so that we can make sure connections are closed
-  }
-
-  async loadTreasuryAccount (ctx) {
-    ctx.treasuryAccountId = constants.TREASURY_ACCOUNT_ID
-    // check to see if the treasure account is in the secrets
-    const accountInfo = await this.accountManager.getAccountKeysFromSecret(ctx.treasuryAccountId, ctx.config.namespace)
-
-    // if it isn't in the secrets we can load genesis key
-    if (accountInfo) {
-      ctx.treasuryPrivateKey = accountInfo.privateKey
-    } else {
-      ctx.treasuryPrivateKey = constants.GENESIS_KEY
-    }
   }
 
   async getAccountInfo (ctx) {
@@ -101,7 +88,6 @@ export class AccountCommand extends BaseCommand {
         this.logger.error(`failed to update account keys for accountId ${ctx.accountInfo.accountId}`)
         return false
       }
-      this.logger.debug(`sent account key update for account ${ctx.accountInfo.accountId}`)
     } else {
       amount = amount || flags.amount.definition.defaultValue
     }
@@ -156,7 +142,7 @@ export class AccountCommand extends BaseCommand {
 
           self.logger.debug('Initialized config', { config })
 
-          await self.loadTreasuryAccount(ctx)
+          ctx.treasuryAccountInfo = await self.accountManager.getTreasuryAccountKeys(ctx.config.namespace)
           await self.loadNodeClient(ctx)
         }
       },
@@ -219,7 +205,7 @@ export class AccountCommand extends BaseCommand {
       {
         title: 'get the account info',
         task: async (ctx, task) => {
-          await self.loadTreasuryAccount(ctx)
+          ctx.treasuryAccountInfo = await self.accountManager.getTreasuryAccountKeys(ctx.config.namespace)
           await self.loadNodeClient(ctx)
           ctx.accountInfo = await self.buildAccountInfo(await self.getAccountInfo(ctx), ctx.config.namespace, ctx.config.privateKey)
         }
@@ -286,7 +272,7 @@ export class AccountCommand extends BaseCommand {
       {
         title: 'get the account info',
         task: async (ctx, task) => {
-          await self.loadTreasuryAccount(ctx)
+          ctx.treasuryAccountInfo = await self.accountManager.getTreasuryAccountKeys(ctx.config.namespace)
           await self.loadNodeClient(ctx)
           self.accountInfo = await self.buildAccountInfo(await self.getAccountInfo(ctx), ctx.config.namespace, false)
           this.logger.showJSON('account info', self.accountInfo)
