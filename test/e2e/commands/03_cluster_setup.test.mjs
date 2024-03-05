0e9bb96df89ce2bdb99ffeee1c2f88da17d977b3
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-03-05 04:28:23 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- test/e2e/commands/03_cluster_setup.test.mjs
+++ test/e2e/commands/03_cluster_setup.test.mjs
@@ -15,21 +15,21 @@
  *
  */
 import {
-	afterAll,
-	afterEach,
-	beforeAll,
-	beforeEach,
-	describe,
-	expect,
-	it
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it
 } from '@jest/globals'
 import {
-	ChartManager,
-	ConfigManager,
-	constants,
-	DependencyManager,
-	Helm,
-	K8
+  ChartManager,
+  ConfigManager,
+  constants,
+  DependencyManager,
+  Helm,
+  K8
 } from '../../../src/core/index.mjs'
 import { getTestCacheDir, testLogger } from '../../test_util.js'
 import path from 'path'
@@ -41,66 +41,66 @@ import {ClusterCommand} from "../../../src/commands/cluster.mjs";
 import {NetworkCommand} from "../../../src/commands/network.mjs";
 
 describe('network commands should work correctly', () => {
-	const defaultTimeout = 20000
-	let networkCmd
-	let accountManager
-	let configManager
-	let k8
-	let helm
-	let chartManager
-	let depManager
-	let argv = {}
+  const defaultTimeout = 20000
+  let networkCmd
+  let accountManager
+  let configManager
+  let k8
+  let helm
+  let chartManager
+  let depManager
+  let argv = {}
 
-	beforeAll(() => {
-		configManager = new ConfigManager(testLogger, path.join(getTestCacheDir('accountCmd'), 'solo.config'))
-		k8 = new K8(configManager, testLogger)
-		accountManager = new AccountManager(testLogger, k8, constants)
-		helm = new Helm(testLogger)
-		chartManager = new ChartManager(helm, testLogger)
-		depManager = new DependencyManager(testLogger)
-		networkCmd = new NetworkCommand({
-			logger: testLogger,
-			helm,
-			k8,
-			chartManager,
-			configManager,
-			depManager
-		})
-	})
+  beforeAll(() => {
+    configManager = new ConfigManager(testLogger, path.join(getTestCacheDir('accountCmd'), 'solo.config'))
+    k8 = new K8(configManager, testLogger)
+    accountManager = new AccountManager(testLogger, k8, constants)
+    helm = new Helm(testLogger)
+    chartManager = new ChartManager(helm, testLogger)
+    depManager = new DependencyManager(testLogger)
+    networkCmd = new NetworkCommand({
+      logger: testLogger,
+      helm,
+      k8,
+      chartManager,
+      configManager,
+      depManager
+    })
+  })
 
-	// afterAll(() => {
-	// 	networkCmd.reset(argv)
-	// })
+  // afterAll(() => {
+  // 	networkCmd.reset(argv)
+  // })
 
-	beforeEach(() => {
-		configManager.reset()
-		argv = {}
-		argv[flags.cacheDir.name] = getTestCacheDir('networkCmd')
-		argv[flags.namespace.name] = 'solo-e2e'
-		argv[flags.clusterName.name] = 'kind-solo-e2e'
-		argv[flags.clusterSetupNamespace.name] = 'solo-e2e-cluster'
-		argv[flags.deployMirrorNode.name] = false
-		argv[flags.deployHederaExplorer.name] = false
-		argv[flags.releaseTag.name] = "v0.42.5" //flags.releaseTag.definition.defaultValue
-		argv[flags.tlsClusterIssuerType.name] = flags.tlsClusterIssuerType.definition.defaultValue
-		argv[flags.hederaExplorerTlsHostName.name] = flags.hederaExplorerTlsHostName.definition.defaultValue
-		argv[flags.enablePrometheusSvcMonitor.name] = flags.enablePrometheusSvcMonitor.definition.defaultValue
-		argv[flags.enableHederaExplorerTls.name] = flags.enableHederaExplorerTls.definition.defaultValue
-		argv[flags.nodeIDs.name] = 'node0,node1,node2'
-		configManager.update(argv, true)
-	})
+  beforeEach(() => {
+    configManager.reset()
+    argv = {}
+    argv[flags.cacheDir.name] = getTestCacheDir('networkCmd')
+    argv[flags.namespace.name] = 'solo-e2e'
+    argv[flags.clusterName.name] = 'kind-solo-e2e'
+    argv[flags.clusterSetupNamespace.name] = 'solo-e2e-cluster'
+    argv[flags.deployMirrorNode.name] = false
+    argv[flags.deployHederaExplorer.name] = false
+    argv[flags.releaseTag.name] = "v0.42.5" //flags.releaseTag.definition.defaultValue
+    argv[flags.tlsClusterIssuerType.name] = flags.tlsClusterIssuerType.definition.defaultValue
+    argv[flags.hederaExplorerTlsHostName.name] = flags.hederaExplorerTlsHostName.definition.defaultValue
+    argv[flags.enablePrometheusSvcMonitor.name] = flags.enablePrometheusSvcMonitor.definition.defaultValue
+    argv[flags.enableHederaExplorerTls.name] = flags.enableHederaExplorerTls.definition.defaultValue
+    argv[flags.nodeIDs.name] = 'node0,node1,node2'
+    configManager.update(argv, true)
+  })
 
-	afterEach(() => {
-		sleep(5).then().catch() // give a few ticks so that connections can close
-	})
+  afterEach(() => {
+    sleep(5).then().catch() // give a few ticks so that connections can close
+  })
 
-	it('network deploy should succeed', async () => {
-		expect.assertions(1)
-		try {
-			await expect(networkCmd.deploy(argv)).resolves.toBeTruthy()
-		} catch (e) {
-			networkCmd.logger.showUserError(e)
-			expect(e).toBeNull()
-		}
-	}, 60000)
+  it('network deploy should succeed', async () => {
+    expect.assertions(1)
+    try {
+      await expect(networkCmd.deploy(argv)).resolves.toBeTruthy()
+    } catch (e) {
+      networkCmd.logger.showUserError(e)
+      expect(e).toBeNull()
+    }
+  }, 60000)
 })
