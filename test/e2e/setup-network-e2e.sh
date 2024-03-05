46ce96e3b7af25518ac45139a635d37cc3905452
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-03-05 10:21:48 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- test/e2e/setup-network-e2e.sh
+++ test/e2e/setup-network-e2e.sh
@@ -3,5 +3,4 @@ readonly KIND_IMAGE="kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23
 
 SOLO_NETWORK_NAMESPACE=solo-network
 SOLO_CLUSTER_SETUP_NAMESPACE=solo-e2e-cluster
-solo init --namespace "${SOLO_NETWORK_NAMESPACE}" -i node0,node1,node2 -t v0.42.5 -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" || exit 1 # cache args for subsequent commands
 
