f4fc5d9c0ed5cbbe025e6a74b8ed31fc7c088882
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-03-06 15:51:55 -0600

    test

--- test/unit/commands/base.test.mjs
+++ test/unit/commands/base.test.mjs
@@ -48,7 +48,7 @@ describe('BaseCommand', () => {
       await expect(baseCmd.run('INVALID_PROGRAM')).rejects.toThrowError()
     })
     it('should succeed during valid program check', async () => {
-      await expect(baseCmd.run('date')).resolves.not.toBeNull()
-    }, 10000)
+      await expect(baseCmd.run('date', true)).resolves.not.toBeNull()
+    })
   })
 })
