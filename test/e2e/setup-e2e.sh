77e2d66b95068f7d07be1f78490a5f6460172416
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-03-11 09:31:04 -0500

    save

--- test/e2e/setup-e2e.sh
+++ test/e2e/setup-e2e.sh
@@ -5,7 +5,7 @@ SOLO_CLUSTER_NAME=solo-e2e
 SOLO_NAMESPACE=solo-e2e
 SOLO_CLUSTER_SETUP_NAMESPACE=solo-e2e-cluster
 kind delete cluster -n "${SOLO_CLUSTER_NAME}" || true
-kind create cluster -n "${SOLO_CLUSTER_NAME}" || exit 1
+kind create cluster -n "${SOLO_CLUSTER_NAME}" --loglevel debug || exit 1
 solo init --namespace "${SOLO_NAMESPACE}" -i node0,node1,node2 -t v0.42.5 -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" || exit 1 # cache args for subsequent commands
 solo cluster setup  || exit 1
 solo network deploy || exit 1
