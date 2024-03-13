e14d1c302cc3a6c33f398ab05020f340add4aa78
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2024-03-14 10:06:01 +1100

    ci: test with fst chart version v0.22.0
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

--- test/e2e/setup-e2e.sh
+++ test/e2e/setup-e2e.sh
@@ -6,6 +6,7 @@ SOLO_NAMESPACE=solo-e2e
 SOLO_CLUSTER_SETUP_NAMESPACE=solo-e2e-cluster
 kind delete cluster -n "${SOLO_CLUSTER_NAME}" || true
 kind create cluster -n "${SOLO_CLUSTER_NAME}" --image "${KIND_IMAGE}" || exit 1
-solo init --namespace "${SOLO_NAMESPACE}" -i node0,node1,node2 -t v0.42.5 -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" || exit 1 # cache args for subsequent commands
+solo init --namespace "${SOLO_NAMESPACE}" -i node0,node1,node2 -t v0.42.5 -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --fst-chart-version v0.22.0 --dev || exit 1 # cache args for subsequent commands
 solo cluster setup  || exit 1
 solo network deploy || exit 1
+helm list --all-namespaces
