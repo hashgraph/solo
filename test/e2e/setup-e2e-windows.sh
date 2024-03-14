6cc4306d923c73529de99a131a5e17f72429846a
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-03-14 18:49:17 -0500

    save

--- test/e2e/setup-e2e-windows.sh
+++ test/e2e/setup-e2e-windows.sh
@@ -4,7 +4,7 @@ SOLO_CLUSTER_NAME=solo-e2e
 SOLO_NAMESPACE=solo-e2e
 SOLO_CLUSTER_SETUP_NAMESPACE=solo-e2e-cluster
 minikube profile list
-minikube start -p ${SOLO_CLUSTER_NAME}
+minikube start -p ${SOLO_CLUSTER_NAME} --driver=docker
 solo init --namespace "${SOLO_NAMESPACE}" -i node0,node1,node2 -t v0.42.5 -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" || exit 1 # cache args for subsequent commands
 solo cluster setup  || exit 1
 solo network deploy || exit 1
