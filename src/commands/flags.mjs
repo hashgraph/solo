d36252fd4713f5a1371fa593c178cf6d369a984b
Jeromy Cannon <jeromy@swirldslabs.com>
2024-06-03 18:41:13 +0100

    added chain id and app name defaults to flags
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/commands/flags.mjs
+++ src/commands/flags.mjs
@@ -247,7 +247,7 @@ export const chainId = {
   name: 'ledger-id',
   definition: {
     describe: 'Ledger ID (a.k.a. Chain ID)',
-    defaultValue: '298', // Ref: https://github.com/hashgraph/hedera-json-rpc-relay#configuration
+    defaultValue: constants.HEDERA_CHAIN_ID, // Ref: https://github.com/hashgraph/hedera-json-rpc-relay#configuration
     alias: 'l',
     type: 'string'
   }
@@ -403,7 +403,7 @@ export const app = {
   name: 'app',
   definition: {
     describe: 'Testing app name',
-    defaultValue: '',
+    defaultValue: constants.HEDERA_APP_NAME,
     type: 'string'
   }
 }
