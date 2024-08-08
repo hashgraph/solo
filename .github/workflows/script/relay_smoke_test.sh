f1e8c013aa0093bcfe97bbb5bfd3bacb8a57b2f1
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-08-08 15:10:52 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/relay_smoke_test.sh
+++ .github/workflows/script/relay_smoke_test.sh
@@ -32,15 +32,12 @@ cat private_key_with_quote.txt | sed '$ s/.$//' > private_key_with_quote_final.t
 cat private_key_without_quote.txt | sed '$ s/.$//' > private_key_without_quote_final.txt
 
 LOCAL_NODE_KEYS=$(cat private_key_with_quote_final.txt)
-CONTRAC_TEST_KEYS=$(cat private_key_without_quote_final.txt)
+CONTRACT_TEST_KEYS=$(cat private_key_without_quote_final.txt)
 
 echo "Add new keys to hardhat.config.js"
 awk '/accounts: \[/ {print; getline; getline; next} 1' test/smoke/hardhat.config.js > test/smoke/hardhat.config.js.tmp
 awk -v new_keys="$LOCAL_NODE_KEYS" '/\],/ {print new_keys; print; next} 1' test/smoke/hardhat.config.js.tmp > test/smoke/hardhat.config.js
 cat test/smoke/hardhat.config.js
-
-echo "Start background transaction"
-watch npm run generate-accounts 3 >> background.log &
 
 #echo "Run smoke test"
 #cd test/smoke
@@ -55,11 +52,13 @@ npx hardhat compile
 
 echo "Build .env file"
 
-
-echo "PRIVATE_KEYS=\"$CONTRAC_TEST_KEYS\"" > .env
+echo "PRIVATE_KEYS=\"$CONTRACT_TEST_KEYS\"" > .env
 echo "RETRY_DELAY=5000 # ms" >> .env
 echo "MAX_RETRY=5" >> .env
-
 cat .env
+
+echo "Start background transaction"
+
+cd ../hedera-local-node;  watch npm run generate-accounts 3 >> background.log &  cd -
 
 npm run hh:test
