f584527e56ba5e7709a2ce9f8f0731dd397cc257
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-08-08 14:52:57 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/relay_smoke_test.sh
+++ .github/workflows/script/relay_smoke_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Starting a test network with a single node"
+echo "Starting test network with a single node"
 
 ./test/e2e/setup-e2e.sh
 solo node keys --gossip-keys --tls-keys --key-format pfx -i node0
@@ -19,28 +19,30 @@ git clone https://github.com/hashgraph/hedera-local-node --branch release-2.29.0
 cd hedera-local-node
 npm install
 
-echo "Generate keys, extract from output and save to key.txt"
+echo "Generate ECDSA keys, extract from output and save to key.txt"
 npm run generate-accounts 3 >> key.log
 sed -n 's/.* - \(0x[0-9a-f]*\) - \(0x[0-9a-f]*\) - .*/\1 \2/p' key.log > key.txt
 
-echo "Only keep the second column of each line of file key.txt"
-awk '{print "\"" $2 "\","}' key.txt > key2.txt
+echo "Only keep the private key, the second column of each line of file key.txt"
+awk '{print "\"" $2 "\","}' key.txt > private_key_with_quote.txt
+awk '{print "" $2 ","}' key.txt > private_key_without_quote.txt
 
 echo "Remove the comma of the last line before add to json file"
-cat key2.txt | sed '$ s/.$//' > key3.txt
+cat private_key_with_quote.txt | sed '$ s/.$//' > private_key_with_quote_final.txt
+cat private_key_without_quote.txt | sed '$ s/.$//' > private_key_without_quote_final.txt
 
-NEW_KEYS=$(cat key3.txt)
+LOCAL_NODE_KEYS=$(cat private_key_with_quote_final.txt)
+CONTRAC_TEST_KEYS=$(cat private_key_without_quote_final.txt)
 
 echo "Add new keys to hardhat.config.js"
 awk '/accounts: \[/ {print; getline; getline; next} 1' test/smoke/hardhat.config.js > test/smoke/hardhat.config.js.tmp
-awk -v new_keys="$NEW_KEYS" '/\],/ {print new_keys; print; next} 1' test/smoke/hardhat.config.js.tmp > test/smoke/hardhat.config.js
+awk -v new_keys="$LOCAL_NODE_KEYS" '/\],/ {print new_keys; print; next} 1' test/smoke/hardhat.config.js.tmp > test/smoke/hardhat.config.js
 cat test/smoke/hardhat.config.js
 
 echo "Start background transaction"
-#watch npm run generate-accounts 3 >> background.log &
+watch npm run generate-accounts 3 >> background.log &
 
-echo "Run smoke test"
-
+#echo "Run smoke test"
 #cd test/smoke
 #npm install
 #npx hardhat test
@@ -53,9 +55,11 @@ npx hardhat compile
 
 echo "Build .env file"
 
-echo "PRIVATE_KEYS=$NEW_KEYS" > .env
+
+echo "PRIVATE_KEYS=\"$CONTRAC_TEST_KEYS\"" > .env
 echo "RETRY_DELAY=5000 # ms" >> .env
 echo "MAX_RETRY=5" >> .env
 
+cat .env
 
 npm run hh:test
