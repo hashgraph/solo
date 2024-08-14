d0bd79aa1e9208192bfa5c8ed1887560480ee4ad
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-08-14 16:29:55 -0500

    revert app verion
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version.mjs
+++ version.mjs
@@ -22,4 +22,4 @@
 export const JAVA_VERSION = '21.0.1+12'
 export const HELM_VERSION = 'v3.14.2'
 export const FST_CHART_VERSION = 'v0.29.1'
-export const HEDERA_PLATFORM_VERSION = 'v0.49.0-alpha.2'
+export const HEDERA_PLATFORM_VERSION = 'v0.53.0-release-0.53.xff7c43d'
