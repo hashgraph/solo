5e04edcf74977336855a14b8a2ad26b449865c37
Jeromy Cannon <jeromy@swirldslabs.com>
2024-08-27 14:37:21 +0100

    don't prompt for local build path
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/commands/node.mjs
+++ src/commands/node.mjs
@@ -1194,7 +1194,8 @@ export class NodeCommand extends BaseCommand {
           // disable the prompts that we don't want to prompt the user for
           prompts.disablePrompts([
             flags.devMode,
-            flags.force
+            flags.force,
+            flags.localBuildPath
           ])
 
           await prompts.execute(task, self.configManager, NodeCommand.REFRESH_FLAGS_LIST)
