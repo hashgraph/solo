5d097d10c042705d31842915a2879fec5772fd3d
JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>
2024-09-05 09:25:09 -0500

    Update test/e2e/commands/node_pem_stop.test.mjs
    Co-authored-by: Jeromy Cannon <jeromy@swirldslabs.com>
    Signed-off-by: JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>

--- test/e2e/commands/node_pem_stop.test.mjs
+++ test/e2e/commands/node_pem_stop.test.mjs
@@ -20,5 +20,5 @@ import { describe } from '@jest/globals'
 import { e2eNodeKeyRefreshTest } from '../e2e_node_util.js'
 
 describe('NodeCommand', () => {
-  e2eNodeKeyRefreshTest('node-cmd-e2e-pem', 'stop')
+  e2eNodeKeyRefreshTest('node-cmd-e2e-pem-stop', 'stop')
 })
