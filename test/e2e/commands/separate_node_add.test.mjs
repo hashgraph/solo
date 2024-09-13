c478f49cbbdaba422f5858b9886e982f3ac751ad
Ivo Yankov <ivo@devlabs.bg>
2024-09-13 17:20:17 +0300

    nit: revert sorting
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- test/e2e/commands/separate_node_add.test.mjs
+++ test/e2e/commands/separate_node_add.test.mjs
@@ -81,7 +81,6 @@ describe('Node add via separated commands should success', () => {
     await nodeCmd.addSubmitTransactions(argvExecute)
     await nodeCmd.addExecute(argvExecute)
     expect(nodeCmd.getUnusedConfigs(NodeCommand.ADD_CONFIGS_NAME)).toEqual([
-      flags.adminKey.constName,
       flags.app.constName,
       flags.chainId.constName,
       flags.devMode.constName,
@@ -89,6 +88,7 @@ describe('Node add via separated commands should success', () => {
       flags.generateTlsKeys.constName,
       flags.gossipEndpoints.constName,
       flags.grpcEndpoints.constName,
+      flags.adminKey.constName,
       'curDate',
       'freezeAdminPrivateKey'
     ])
