c594413a91a78fe542c3ee3dbc79ff98820f3128
Jeromy Cannon <jeromy@swirldslabs.com>
2024-10-03 18:14:52 +0100

    add jsdoc markdown support
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/commands/node.mjs
+++ src/commands/node.mjs
@@ -357,6 +357,8 @@ export class NodeCommand extends BaseCommand {
 
   /**
    * stops and closes the port forwards
+   * - calls the accountManager.close()
+   * - for all portForwards, calls k8.stopPortForward(srv)
    * @returns {Promise<void>}
    */
   async close () {
