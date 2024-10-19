a2e85460f4d2bd7a10e01c16febb0df51e792a82
Jeromy Cannon <jeromy@swirldslabs.com>
2024-10-19 20:13:16 +0100

    add all needed argvs
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- .github/workflows/script/update_md.sh
+++ .github/workflows/script/update_md.sh
@@ -9,37 +9,31 @@ echo "Perform the following kind and solo commands and save output to environmen
 kind create cluster -n "${SOLO_CLUSTER_NAME}" 2>&1 | tee create-cluster.log
 export KIND_CREATE_CLUSTER_OUTPUT=$( cat create-cluster.log | tee test.log )
 
-solo init -i node1,node2,node3 -n "${SOLO_NAMESPACE}" -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" | tee init.log
+solo init | tee init.log
 export SOLO_INIT_OUTPUT=$( cat init.log | tee test.log )
 
-#solo node keys --gossip-keys --tls-keys </dev/null | cat
-#
-#solo node keys --gossip-keys --tls-keys
-#
-#( npm run solo node keys --gossip-keys --tls-keys )
-
-solo node keys --gossip-keys --tls-keys | tee keys.log
+solo node keys --gossip-keys --tls-keys -i node1,node2,node3 | tee keys.log
 export SOLO_NODE_KEY_PEM_OUTPUT=$( cat keys.log | tee test.log )
 
-solo cluster setup | tee cluster-setup.log
+solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" | tee cluster-setup.log
 export SOLO_CLUSTER_SETUP_OUTPUT=$( cat cluster-setup.log | tee test.log )
 
-solo network deploy | tee network-deploy.log
+solo network deploy -i node1,node2,node3 -n "${SOLO_NAMESPACE}" | tee network-deploy.log
 export SOLO_NETWORK_DEPLOY_OUTPUT=$( cat network-deploy.log | tee test.log )
 
-solo node setup | tee node-setup.log
+solo node setup -i node1,node2,node3 -n "${SOLO_NAMESPACE}" | tee node-setup.log
 export SOLO_NODE_SETUP_OUTPUT=$( cat node-setup.log | tee test.log )
 
-solo node start | tee node-start.log
+solo node start -i node1,node2,node3 -n "${SOLO_NAMESPACE}" | tee node-start.log
 export SOLO_NODE_START_OUTPUT=$( cat node-start.log | tee test.log )
 
-solo mirror-node deploy | tee mirror-node-deploy.log
+solo mirror-node deploy -n "${SOLO_NAMESPACE}" | tee mirror-node-deploy.log
 export SOLO_MIRROR_NODE_DEPLOY_OUTPUT=$( cat mirror-node-deploy.log | tee test.log )
 
-solo relay deploy -i node1,node2 | tee relay-deploy.log
+solo relay deploy -i node1,node2 -n "${SOLO_NAMESPACE}" | tee relay-deploy.log
 export SOLO_RELAY_DEPLOY_OUTPUT=$( cat relay-deploy.log | tee test.log )
 
-solo relay deploy | tee relay-deploy.log
+solo relay deploy -i node1,node2,node3 -n "${SOLO_NAMESPACE}" | tee relay-deploy.log
 export SOLO_RELAY_DEPLAY_OUTPUT=$( cat relay-deploy.log | tee test.log )
 echo "Generate README.md"
 
