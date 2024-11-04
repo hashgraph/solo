1223205a7cb73f500d62c7d112a6f6ea1722cad2
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-11-04 11:10:07 -0600

    update chart path
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/network.ts
+++ src/commands/network.ts
@@ -281,7 +281,7 @@ export class NetworkCommand extends BaseCommand {
           await this.chartManager.install(
             config.namespace,
             constants.SOLO_DEPLOYMENT_CHART,
-            constants.SOLO_TESTING_CHART_URL,
+            constants.SOLO_TESTING_CHART_URL + constants.SOLO_DEPLOYMENT_CHART,
             config.soloChartVersion,
             config.valuesArg)
         }
