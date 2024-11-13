84d52bd91bfa90a641d50e9bfcf6e26330aafcaa
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-11-13 16:17:25 -0600

    change local node version
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/relay_smoke_test.sh
+++ .github/workflows/script/relay_smoke_test.sh
@@ -29,7 +29,7 @@ if [ -d "hedera-local-node" ]; then
   echo "Directory hedera-local-node exists."
 else
   echo "Directory hedera-local-node does not exist."
-  git clone https://github.com/hashgraph/hedera-local-node --branch release-2.29.0
+  git clone https://github.com/hashgraph/hedera-local-node --branch release-2.32.1
 fi
 
 cd hedera-local-node
