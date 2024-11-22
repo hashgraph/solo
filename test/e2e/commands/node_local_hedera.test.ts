9cede767fc66c50567996351b5fbe5b5c0ecd357
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-11-22 11:19:49 -0600

    add logic to read back balance
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- test/e2e/commands/node_local_hedera.test.ts
+++ test/e2e/commands/node_local_hedera.test.ts
@@ -26,6 +26,8 @@ import { getNodeLogs, sleep } from '../../../src/core/helpers.js'
 import { MINUTES, SOLO_LOGS_DIR } from '../../../src/core/constants.js'
 import type { K8 } from '../../../src/core/index.js'
 import path from 'path'
+import { expect } from 'chai'
+import { AccountBalanceQuery, AccountCreateTransaction, Hbar, HbarUnit, PrivateKey } from '@hashgraph/sdk'
 
 const LOCAL_HEDERA = 'local-hedera-app'
 const argv = getDefaultArgv()
@@ -45,11 +47,30 @@ argv[flags.namespace.name] = LOCAL_HEDERA
 e2eTestSuite(LOCAL_HEDERA, argv, undefined, undefined, undefined, undefined, undefined, undefined, true, (bootstrapResp) => {
   const nodeCmd = bootstrapResp.cmd.nodeCmd
   const accountCmd = bootstrapResp.cmd.accountCmd
+  const accountManager = bootstrapResp.manager.accountManager
   describe('Node for hedera app should have started successfully', () => {
     hederaK8 = bootstrapResp.opts.k8
 
     it('save the state and restart the node with saved state', async function () {
-      // create some transactions to save more round of states
+      // create an account so later we can verify its balance after restart
+      await accountManager.loadNodeClient(LOCAL_HEDERA)
+      const privateKey = PrivateKey.generate()
+      // get random integer between 100 and 1000
+      const amount = Math.floor(Math.random() * (1000 - 100) + 100)
+
+      const newAccount = await new AccountCreateTransaction()
+      .setKey(privateKey)
+      .setInitialBalance(Hbar.from(amount, HbarUnit.Hbar))
+      .execute(accountManager._nodeClient)
+
+      // Get the new account ID
+      const getReceipt = await newAccount.getReceipt(accountManager._nodeClient)
+      const accountInfo = {
+        accountId: getReceipt.accountId.toString(),
+        balance: amount
+      }
+
+      // create more transactions to save more round of states
       await accountCmd.create(argv)
       await sleep(3)
       await accountCmd.create(argv)
@@ -61,6 +82,14 @@ e2eTestSuite(LOCAL_HEDERA, argv, undefined, undefined, undefined, undefined, und
 
       argv[flags.stateFile.name] = path.join(SOLO_LOGS_DIR, LOCAL_HEDERA, 'network-node1-0-state.zip')
       await nodeCmd.handlers.start(argv)
+
+      // check balance of accountInfo.accountId
+      await accountManager.loadNodeClient(LOCAL_HEDERA)
+      const balance = await new AccountBalanceQuery()
+      .setAccountId(accountInfo.accountId)
+      .execute(accountManager._nodeClient)
+
+      expect(balance.hbars).to.be.eql(Hbar.from(accountInfo.balance, HbarUnit.Hbar))
     }).timeout(10 * MINUTES)
 
     it('get the logs and delete the namespace', async function () {
