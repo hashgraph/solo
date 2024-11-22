440fa25443f97b3ce606932dde7285221b0bd464
instamenta <instamenta@abv.bg>
2024-11-22 15:55:13 +0200

    fix localconfig formatting and validation
    Signed-off-by: instamenta <instamenta@abv.bg>

--- src/core/config/local_config.ts
+++ src/core/config/local_config.ts
@@ -21,134 +21,135 @@ import { flags } from '../../commands/index.js'
 import { MissingArgumentError, SoloError } from '../errors.js'
 import { promptDeploymentClusters, promptDeploymentName, promptUserEmailAddress } from '../../commands/prompts.js'
 import type { ListrTask, ListrTaskWrapper } from 'listr2'
-import type { Deployments, LocalConfigData, DeploymentStructure} from './local_config_data.js'
+import type { Deployments, LocalConfigData, DeploymentStructure } from './local_config_data.js'
 import type { SoloLogger } from '../logging.js'
 import type { EmailAddress, Namespace } from './remote/types.js'
 import type { ConfigManager } from '../config_manager.js'
 import { IsDeployments } from '../validator_decorators.js'
 
 export class LocalConfig implements LocalConfigData {
-    @IsNotEmpty()
-    @IsEmail()
-    public userEmailAddress: EmailAddress
+  @IsNotEmpty()
+  @IsEmail()
+  public userEmailAddress: EmailAddress
 
-    // The string is the name of the deployment, will be used as the namespace,
-    // so it needs to be available in all targeted clusters
-    @IsNotEmpty()
-    @IsObject()
-    @IsDeployments()
-    public deployments: Deployments
+  // The string is the name of the deployment, will be used as the namespace,
+  // so it needs to be available in all targeted clusters
+  @IsNotEmpty()
+  @IsObject()
+  @IsDeployments()
+  public deployments: Deployments
 
-    @IsNotEmpty()
-    @IsString()
-    public currentDeploymentName : Namespace
+  @IsNotEmpty()
+  @IsString()
+  public currentDeploymentName: Namespace
 
-    private readonly skipPromptTask: boolean = false
+  private readonly skipPromptTask: boolean = false
 
-    public constructor (
-      private readonly filePath: string,
-      private readonly logger: SoloLogger,
-      private readonly configManager: ConfigManager,
-    ) {
-        if (!filePath || filePath === '') throw new MissingArgumentError('a valid filePath is required')
-        if (!logger) throw new Error('An instance of core/SoloLogger is required')
+  public constructor (
+    private readonly filePath: string,
+    private readonly logger: SoloLogger,
+    private readonly configManager: ConfigManager,
+  ) {
+    if (!filePath || filePath === '') throw new MissingArgumentError('a valid filePath is required')
+    if (!logger) throw new Error('An instance of core/SoloLogger is required')
 
-        const allowedKeys = ['userEmailAddress', 'deployments', 'currentDeploymentName', 'clusterMappings']
-        if (this.configFileExists()) {
-            const fileContent = fs.readFileSync(filePath, 'utf8')
-            const parsedConfig = yaml.parse(fileContent)
+    const allowedKeys = ['userEmailAddress', 'deployments', 'currentDeploymentName']
+    if (this.configFileExists()) {
+      const fileContent = fs.readFileSync(filePath, 'utf8')
+      const parsedConfig = yaml.parse(fileContent)
 
-            for(const key in parsedConfig) {
-                if (!allowedKeys.includes(key)) {
-                    throw new SoloError('Validation of local config failed')
-                }
-                this[key] = parsedConfig[key]
-            }
-
-            this.validate()
-            this.skipPromptTask = true
+      for (const key in parsedConfig) {
+        if (!allowedKeys.includes(key)) {
+          throw new SoloError('Validation of local config failed')
         }
+        this[key] = parsedConfig[key]
+      }
+
+      this.validate()
+      this.skipPromptTask = true
+    }
+  }
+
+  private validate (): void {
+    const genericMessage = 'Validation of local config failed'
+    const errors = validateSync(this, {})
+
+    if (errors.length) {
+      throw new SoloError(genericMessage)
     }
 
-    private validate (): void {
-        const genericMessage = 'Validation of local config failed'
-        const errors = validateSync(this, {})
+    try {
+      // Custom validations:
+      if (!this.deployments[this.currentDeploymentName]) {
+        throw new SoloError(genericMessage)
+      }
+    } catch (e: any) {
+      throw new SoloError(genericMessage)
+    }
+  }
 
-        if (errors.length) {
-            throw new SoloError(genericMessage)
+  public setUserEmailAddress (emailAddress: EmailAddress): this {
+    this.userEmailAddress = emailAddress
+    this.validate()
+    return this
+  }
+
+  public setDeployments (deployments: Deployments): this {
+    this.deployments = deployments
+    this.validate()
+    return this
+  }
+
+  public setCurrentDeployment (deploymentName: string): this {
+    this.currentDeploymentName = deploymentName
+    this.validate()
+    return this
+  }
+
+  public getCurrentDeployment (): DeploymentStructure {
+    return this.deployments[this.currentDeploymentName]
+  }
+
+  public configFileExists (): boolean {
+    return fs.existsSync(this.filePath)
+  }
+
+  public async write (): Promise<void> {
+    const yamlContent = yaml.stringify({
+      userEmailAddress: this.userEmailAddress,
+      deployments: this.deployments,
+      currentDeploymentName: this.currentDeploymentName
+    })
+    await fs.promises.writeFile(this.filePath, yamlContent)
+    this.logger.info(`Wrote local config to ${this.filePath}`)
+  }
+
+  public promptLocalConfigTask (): ListrTask<any, any, any> {
+    const self = this
+
+    return {
+      title: 'Prompt local configuration',
+      skip: this.skipPromptTask,
+      task: async (_: any, task: ListrTaskWrapper<any, any, any>): Promise<void> => {
+        let userEmailAddress = self.configManager.getFlag<EmailAddress>(flags.userEmailAddress)
+        if (!userEmailAddress) userEmailAddress = await promptUserEmailAddress(task, userEmailAddress)
+
+        let deploymentName = self.configManager.getFlag<Namespace>(flags.deploymentName)
+        if (!deploymentName) deploymentName = await promptDeploymentName(task, deploymentName)
+
+        let deploymentClusters = self.configManager.getFlag<string>(flags.deploymentClusters)
+        if (!deploymentClusters) deploymentClusters = await promptDeploymentClusters(task, deploymentClusters)
+
+        const deployments: Deployments = {
+          [deploymentName]: {clusters: deploymentClusters.split(',')}
         }
 
-        try {
-            // Custom validations:
-            if (!this.deployments[this.currentDeploymentName]) {
-                throw new SoloError(genericMessage)
-            }
-        }
-        catch(e: any) { throw new SoloError(genericMessage) }
-    }
-
-    public setUserEmailAddress (emailAddress: EmailAddress): this {
-        this.userEmailAddress = emailAddress
-        this.validate()
-        return this
-    }
-
-    public setDeployments (deployments: Deployments): this {
+        this.userEmailAddress = userEmailAddress
         this.deployments = deployments
-        this.validate()
-        return this
-    }
-
-    public setCurrentDeployment (deploymentName: string): this {
         this.currentDeploymentName = deploymentName
         this.validate()
-        return this
+        await this.write()
+      }
     }
-
-    public getCurrentDeployment (): DeploymentStructure {
-        return this.deployments[this.currentDeploymentName]
-    }
-
-    public configFileExists (): boolean {
-        return fs.existsSync(this.filePath)
-    }
-
-    public async write (): Promise<void> {
-        const yamlContent = yaml.stringify({
-            userEmailAddress: this.userEmailAddress,
-            deployments: this.deployments,
-            currentDeploymentName: this.currentDeploymentName
-        })
-        await fs.promises.writeFile(this.filePath, yamlContent)
-        this.logger.info(`Wrote local config to ${this.filePath}`)
-    }
-
-    public promptLocalConfigTask (): ListrTask<any, any, any>  {
-        const self = this
-
-        return {
-            title: 'Prompt local configuration',
-            skip: this.skipPromptTask,
-            task: async (_: any, task: ListrTaskWrapper<any, any, any>): Promise<void> => {
-                let userEmailAddress = self.configManager.getFlag<EmailAddress>(flags.userEmailAddress)
-                if (!userEmailAddress) userEmailAddress = await promptUserEmailAddress(task, userEmailAddress)
-
-                let deploymentName = self.configManager.getFlag<Namespace>(flags.deploymentName)
-                if (!deploymentName) deploymentName = await promptDeploymentName(task, deploymentName)
-
-                let deploymentClusters = self.configManager.getFlag<string>(flags.deploymentClusters)
-                if (!deploymentClusters) deploymentClusters = await promptDeploymentClusters(task, deploymentClusters)
-
-                const deployments: Deployments = {
-                    [deploymentName]: { clusters: deploymentClusters.split(',') }
-                }
-
-                this.userEmailAddress = userEmailAddress
-                this.deployments = deployments
-                this.currentDeploymentName = deploymentName
-                this.validate()
-                await this.write()
-            }
-        }
-    }
+  }
 }
--- test/e2e/e2e_node_util.ts
+++ test/e2e/e2e_node_util.ts
@@ -32,7 +32,7 @@ import { MINUTES, SECONDS } from '../../src/core/constants.js'
 import type { NodeAlias } from '../../src/types/aliases.js'
 import type { ListrTaskWrapper } from 'listr2'
 import { ConfigManager, type K8 } from '../../src/core/index.js'
-import type { NodeCommand } from '../../src/commands/node/index.js'
+import { type NodeCommand } from '../../src/commands/node/index.js'
 
 export function e2eNodeKeyRefreshTest (testName: string, mode: string, releaseTag = HEDERA_PLATFORM_VERSION_TAG) {
   const namespace = testName
