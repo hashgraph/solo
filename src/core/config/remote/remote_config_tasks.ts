a5d10857466370433abe0f23d49639dc5d5d15e1
Jeromy Cannon <jeromy@swirldslabs.com>
2024-12-05 18:16:09 +0000

    down to 6 circular dependencies
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/core/config/remote/remote_config_tasks.ts
+++ src/core/config/remote/remote_config_tasks.ts
@@ -14,14 +14,8 @@
  * limitations under the License.
  *
  */
-import {ConsensusNodeComponent} from './components/consensus_node_component.js';
-import {EnvoyProxyComponent} from './components/envoy_proxy_component.js';
-import {HaProxyComponent} from './components/ha_proxy_component.js';
-import {ConsensusNodeStates} from './enumerations.js';
-
 import type {ListrTask} from 'listr2';
 import type {BaseCommand} from '../../../commands/base.js';
-import type {NetworkCommand} from '../../../commands/network.js';
 import type {DeploymentCommand} from '../../../commands/deployment.js';
 
 /**
@@ -42,40 +36,5 @@ export class RemoteConfigTasks {
   /** Creates remote config. */
   public static createRemoteConfig(this: DeploymentCommand): ListrTask<any, any, any> {
     return this.remoteConfigManager.buildCreateTask();
-  }
-
-  /* ----------- Component Modifying ----------- */
-
-  /** Adds the consensus node, envoy and haproxy components to remote config.  */
-  public static addNodesAndProxies(this: NetworkCommand): ListrTask<any, any, any> {
-    return {
-      title: 'Add node and proxies to remote config',
-      skip: (): boolean => !this.remoteConfigManager.isLoaded(),
-      task: async (ctx): Promise<void> => {
-        const {
-          config: {namespace, nodeAliases},
-        } = ctx;
-        const cluster = this.remoteConfigManager.currentCluster;
-
-        await this.remoteConfigManager.modify(async remoteConfig => {
-          for (const nodeAlias of nodeAliases) {
-            remoteConfig.components.add(
-              nodeAlias,
-              new ConsensusNodeComponent(nodeAlias, cluster, namespace, ConsensusNodeStates.INITIALIZED),
-            );
-
-            remoteConfig.components.add(
-              `envoy-${nodeAlias}`,
-              new EnvoyProxyComponent(`envoy-${nodeAlias}`, cluster, namespace),
-            );
-
-            remoteConfig.components.add(
-              `haproxy-${nodeAlias}`,
-              new HaProxyComponent(`haproxy-${nodeAlias}`, cluster, namespace),
-            );
-          }
-        });
-      },
-    };
   }
 }
