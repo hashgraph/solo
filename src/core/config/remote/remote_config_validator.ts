99b0e17e986f3bfcd38cb28b8d96de779d9f899e
instamenta <instamenta@abv.bg>
2024-12-05 15:50:09 +0200

    added tests for validator class
    Signed-off-by: instamenta <instamenta@abv.bg>

--- src/core/config/remote/remote_config_validator.ts
+++ src/core/config/remote/remote_config_validator.ts
@@ -15,110 +15,126 @@
  *
  */
 import * as constants from '../../constants.js';
+import {SoloError} from '../../errors.js';
 
 import type {K8} from '../../k8.js';
 import type {ComponentsDataWrapper} from './components_data_wrapper.js';
+import type {BaseComponent} from './components/index.js';
 
+/**
+ * Static class is used to validate that components in the remote config
+ * are present in the kubernetes cluster, and throw errors if there is mismatch.
+ */
 export class RemoteConfigValidator {
+  /**
+   * Gathers together and handles validation of all components.
+   *
+   * @param components - components which to validate.
+   * @param k8 - to validate the elements.
+   * TODO: Make compatible with multi-cluster K8 implementation
+   */
   public static async validateComponents(components: ComponentsDataWrapper, k8: K8): Promise<void> {
     await Promise.all([
-      RemoteConfigValidator.validateRelays(components, k8),
-      RemoteConfigValidator.validateHaProxies(components, k8),
-      RemoteConfigValidator.validateMirrorNodes(components, k8),
-      RemoteConfigValidator.validateEnvoyProxies(components, k8),
-      RemoteConfigValidator.validateConsensusNodes(components, k8),
-      RemoteConfigValidator.validateMirrorNodeExplorers(components, k8),
+      ...RemoteConfigValidator.validateRelays(components, k8),
+      ...RemoteConfigValidator.validateHaProxies(components, k8),
+      ...RemoteConfigValidator.validateMirrorNodes(components, k8),
+      ...RemoteConfigValidator.validateEnvoyProxies(components, k8),
+      ...RemoteConfigValidator.validateConsensusNodes(components, k8),
+      ...RemoteConfigValidator.validateMirrorNodeExplorers(components, k8),
     ]);
   }
 
-  private static async validateRelays(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.relays).map(async component => {
-        try {
-          await k8.getPodsByLabel([constants.SOLO_RELAY_LABEL]);
-        } catch (e) {
-          throw new Error(
-            `Relay in remote config with name ${component.name} was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
-    );
+  private static validateRelays(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.relays).map(async component => {
+      try {
+        const pods = await k8.getPodsByLabel([constants.SOLO_RELAY_LABEL]);
+
+        // to return the generic error message
+        if (!pods.length) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('Relay', component, e);
+      }
+    });
   }
 
-  private static async validateHaProxies(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.haProxies).map(async component => {
-        try {
-          await k8.getPodByName(component.name);
-        } catch (e) {
-          throw new Error(
-            `HaProxy in remote config with name ${component.name} was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
-    );
+  private static validateHaProxies(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.haProxies).map(async component => {
+      try {
+        const pod = await k8.getPodByName(component.name);
+
+        // to return the generic error message
+        if (!pod) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('HaProxy', component, e);
+      }
+    });
   }
 
-  private static async validateMirrorNodes(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.mirrorNodes).map(async component => {
-        try {
-          await k8.getPodsByLabel(constants.SOLO_HEDERA_MIRROR_IMPORTER);
-        } catch (e) {
-          throw new Error(
-            `Mirror node in remote config with name ${component.name} was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
-    );
+  private static validateMirrorNodes(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.mirrorNodes).map(async component => {
+      try {
+        const pods = await k8.getPodsByLabel(constants.SOLO_HEDERA_MIRROR_IMPORTER);
+
+        // to return the generic error message
+        if (!pods.length) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('Mirror node', component, e);
+      }
+    });
   }
 
-  private static async validateEnvoyProxies(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.envoyProxies).map(async component => {
-        try {
-          await k8.getPodByName(component.name);
-        } catch (e) {
-          throw new Error(
-            `Envoy proxy in remote config with name ${component.name} was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
-    );
+  private static validateEnvoyProxies(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.envoyProxies).map(async component => {
+      try {
+        const pod = await k8.getPodByName(component.name);
+
+        // to return the generic error message
+        if (!pod) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('Envoy proxy', component, e);
+      }
+    });
   }
 
-  private static async validateConsensusNodes(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.consensusNodes).map(async component => {
-        try {
-          await k8.getPodByName(component.name);
-        } catch (e) {
-          throw new Error(
-            `Consensus node in remote config with name ${component.name} was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
-    );
+  private static validateConsensusNodes(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.consensusNodes).map(async component => {
+      try {
+        const pod = await k8.getPodByName(component.name);
+
+        // to return the generic error message
+        if (!pod) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('Consensus node', component, e);
+      }
+    });
   }
 
-  private static async validateMirrorNodeExplorers(components: ComponentsDataWrapper, k8: K8): Promise<void> {
-    await Promise.all(
-      Object.values(components.mirrorNodeExplorers).map(async component => {
-        try {
-          await k8.getPodsByLabel([constants.SOLO_HEDERA_EXPLORER_LABEL]);
-        } catch (e) {
-          throw new Error(
-            `Mirror node explorer in remote config with name ${component.name}` +
-              ` was not found in namespace ${component.namespace}`,
-            e,
-          );
-        }
-      }),
+  private static validateMirrorNodeExplorers(components: ComponentsDataWrapper, k8: K8): Promise<void>[] {
+    return Object.values(components.mirrorNodeExplorers).map(async component => {
+      try {
+        const pods = await k8.getPodsByLabel([constants.SOLO_HEDERA_EXPLORER_LABEL]);
+
+        // to return the generic error message
+        if (!pods.length) throw new Error('Pod not found');
+      } catch (e) {
+        RemoteConfigValidator.throwValidationError('Mirror node explorer', component, e);
+      }
+    });
+  }
+
+  /**
+   * Generic handler that throws errors.
+   *
+   * @param type - name to display in error message
+   * @param component - component which is not found in the cluster
+   * @param e - original error for the kube client
+   */
+  private static throwValidationError(type: string, component: BaseComponent, e: Error | unknown): never {
+    throw new SoloError(
+      `${type} in remote config with name ${component.name} ` +
+        `was not found in namespace: ${component.namespace}, cluster: ${component.cluster}`,
+      e,
+      {component: component.toObject()},
     );
   }
 }
