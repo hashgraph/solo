81d89c431b6a174587232d599851a3df5af4a6a7
Jeffrey Tang <jeffrey@swirldslabs.com>
2024-12-16 13:36:59 -0600

    Add test
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

---
+++ .github/workflows/script/gcs_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -eo pipefail
+
+source .github/workflows/script/helper.sh
+
+if [ -z "${GCS_ACCESS_KEY}" ]; then
+  echo "GCS_ACCESS_KEY is not set. Exiting..."
+  exit 1
+fi
+
+if [ -z "${GCS_SECRET_KEY}" ]; then
+  echo "GCS_SECRET_KEY is not set. Exiting..."
+  exit 1
+fi
+
+echo "Generate GCS credentials to file gcs_values.yaml"
+echo "cloud:" > gcs_values.yaml
+echo "  gcs:" >> gcs_values.yaml
+echo "    enabled: true" >> gcs_values.yaml
+echo '    GCS_ACCESS_KEY: "'${GCS_ACCESS_KEY}'"' >> gcs_values.yaml
+echo '    GCS_SECRET_KEY: "'${GCS_SECRET_KEY}'"' >> gcs_values.yaml
+
+
+SOLO_CLUSTER_NAME=solo-e2e
+SOLO_NAMESPACE=solo-e2e
+SOLO_CLUSTER_SETUP_NAMESPACE=solo-setup
+
+kind create cluster -n "${SOLO_CLUSTER_NAME}"
+solo init
+solo cluster setup --chart-dir /Users/jeffrey/solo-charts/charts -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
+solo node keys --gossip-keys --tls-keys -i node1,node2
+solo network deploy --chart-dir /Users/jeffrey/solo-charts/charts -i node1,node2 -n "${SOLO_NAMESPACE}" -f gcs_values.yaml
+solo node setup -i node1,node2 -n "${SOLO_NAMESPACE}"
+solo node start -i node1,node2 -n "${SOLO_NAMESPACE}"
+solo mirror-node deploy --namespace "${SOLO_NAMESPACE}"
+
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/haproxy-node1-svc 50211:50211 > /dev/null 2>&1 &
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/hedera-explorer 8080:80 > /dev/null 2>&1 &
+
+cd ..; create_test_account ; cd -
+
+node examples/create-topic.js
--- examples/create-topic.js
+++ examples/create-topic.js
@@ -14,9 +14,18 @@
  * limitations under the License.
  *
  */
-import {Wallet, LocalProvider, TopicCreateTransaction, TopicMessageSubmitTransaction} from '@hashgraph/sdk';
+import {
+  Wallet,
+  LocalProvider,
+  TopicCreateTransaction,
+  TopicMessageSubmitTransaction,
+  AccountCreateTransaction,
+  PrivateKey,
+  Hbar,
+} from '@hashgraph/sdk';
 
 import dotenv from 'dotenv';
+import http from 'http';
 
 dotenv.config();
 
@@ -30,12 +39,11 @@ async function main() {
 
   const wallet = new Wallet(process.env.OPERATOR_ID, process.env.OPERATOR_KEY, provider);
 
+  const TEST_MESSAGE = 'Hello World';
   try {
-    console.log('before create topic');
     // create topic
     let transaction = await new TopicCreateTransaction().freezeWithSigner(wallet);
     transaction = await transaction.signWithSigner(wallet);
-    console.log('after sign transaction');
     const createResponse = await transaction.executeWithSigner(wallet);
     const createReceipt = await createResponse.getReceiptWithSigner(wallet);
 
@@ -44,7 +52,7 @@ async function main() {
     // send one message
     let topicMessageSubmitTransaction = await new TopicMessageSubmitTransaction({
       topicId: createReceipt.topicId,
-      message: 'Hello World',
+      message: TEST_MESSAGE,
     }).freezeWithSigner(wallet);
     topicMessageSubmitTransaction = await topicMessageSubmitTransaction.signWithSigner(wallet);
     const sendResponse = await topicMessageSubmitTransaction.executeWithSigner(wallet);
@@ -52,10 +60,63 @@ async function main() {
     const sendReceipt = await sendResponse.getReceiptWithSigner(wallet);
 
     console.log(`topic sequence number = ${sendReceipt.topicSequenceNumber.toString()}`);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // send a create account transaction to push record stream files to mirror node
+    const newKey = PrivateKey.generate();
+    let accountCreateTransaction = await new AccountCreateTransaction()
+      .setInitialBalance(new Hbar(10))
+      .setKey(newKey.publicKey)
+      .freezeWithSigner(wallet);
+    accountCreateTransaction = await accountCreateTransaction.signWithSigner(wallet);
+    const accountCreationResponse = await accountCreateTransaction.executeWithSigner(wallet);
+    const accountCreationReceipt = await accountCreationResponse.getReceiptWithSigner(wallet);
+    console.log(`account id = ${accountCreationReceipt.accountId.toString()}`);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Check submit message result should success
+    const queryURL = `http://localhost:8080/api/v1/topics/${createReceipt.topicId}/messages`;
+    let received = false;
+    let receivedMessage = '';
+
+    // wait until the transaction reached consensus and retrievable from the mirror node API
+    let retry = 0;
+    while (!received && retry < 10) {
+      const req = http.request(queryURL, {method: 'GET', timeout: 100, headers: {Connection: 'close'}}, res => {
+        res.setEncoding('utf8');
+        res.on('data', chunk => {
+          // convert chunk to json object
+          const obj = JSON.parse(chunk);
+          if (obj.messages.length === 0) {
+            console.log('No messages yet');
+          } else {
+            // convert message from base64 to utf-8
+            const base64 = obj.messages[0].message;
+            const buff = Buffer.from(base64, 'base64');
+            receivedMessage = buff.toString('utf-8');
+            console.log(`Received message: ${receivedMessage}`);
+            received = true;
+          }
+        });
+      });
+      req.on('error', e => {
+        console.log(`problem with request: ${e.message}`);
+      });
+      req.end(); // make the request
+      // wait and try again
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      retry++;
+    }
+    if (receivedMessage === TEST_MESSAGE) {
+      console.log('Message received successfully');
+    } else {
+      console.error('Message received but not match');
+    }
   } catch (error) {
     console.error(error);
   }
-
   provider.close();
 }
 
