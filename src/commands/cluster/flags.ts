a9ba0c4451c511333bf4624a724252edc3752db3
Ivo Yankov <ivo@devlabs.bg>
2025-01-07 15:11:47 +0200

    chore: Merge the subcommands from context into cluster and get rid of the context command (#1111)
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/commands/cluster/flags.ts
+++ src/commands/cluster/flags.ts
@@ -17,6 +17,34 @@
 
 import {Flags as flags} from '../flags.js';
 
+export const DEFAULT_FLAGS = {
+  requiredFlags: [],
+  requiredFlagsWithDisabledPrompt: [],
+  optionalFlags: [],
+};
+
+export const SETUP_FLAGS = {
+  requiredFlags: [],
+  requiredFlagsWithDisabledPrompt: [],
+  optionalFlags: [
+    flags.chartDirectory,
+    flags.clusterName,
+    flags.clusterSetupNamespace,
+    flags.deployCertManager,
+    flags.deployCertManagerCrds,
+    flags.deployMinio,
+    flags.deployPrometheusStack,
+    flags.quiet,
+    flags.soloChartVersion,
+  ],
+};
+
+export const RESET_FLAGS = {
+  requiredFlags: [],
+  requiredFlagsWithDisabledPrompt: [],
+  optionalFlags: [flags.clusterName, flags.clusterSetupNamespace, flags.force, flags.quiet],
+};
+
 export const USE_FLAGS = {
   requiredFlags: [],
   requiredFlagsWithDisabledPrompt: [],
