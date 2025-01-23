fc294b6007bee1196068885a7e83f357a840ce76
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-01-23 09:45:02 -0600

    add flag for mirror downloader prefix
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/gcs_test.sh
+++ .github/workflows/script/gcs_test.sh
@@ -34,6 +34,11 @@ fi
 if [ -z "${GCP_SERVICE_ACCOUNT_TOKEN}" ]; then
   echo "GCP_SERVICE_ACCOUNT_TOKEN is not set. Exiting..."
   exit 1
+if [ -z "${PREFIX}" ]; then
+  echo "PREFIX is not set"
+else
+  echo "Using PREFIX: ${PREFIX}"
+  BUCKET_PREFIX_OPTION="--storage-bucket-prefix ${PREFIX}"
 fi
 
 echo "${GCP_SERVICE_ACCOUNT_TOKEN}" > gcp_service_account.json
@@ -54,7 +59,7 @@ npm run solo-test -- node keys --gossip-keys --tls-keys -i node1
 npm run solo-test -- network deploy -i node1 -n "${SOLO_NAMESPACE}" \
   --storage-endpoint "https://storage.googleapis.com" \
   --storage-access-key "${GCS_ACCESS_KEY}" --storage-secrets "${GCS_SECRET_KEY}" \
-  --storage-type "${storageType}" --storage-bucket "${streamBucket}" \
+  --storage-type "${storageType}" --storage-bucket "${streamBucket}" $BUCKET_PREFIX_OPTION \
   --backup-bucket "${streamBackupBucket}" \
   --google-credential gcp_service_account.json
 
@@ -63,10 +68,14 @@ npm run solo-test -- node start -i node1 -n "${SOLO_NAMESPACE}"
 npm run solo-test -- mirror-node deploy --namespace "${SOLO_NAMESPACE}" \
   --storage-endpoint "https://storage.googleapis.com" \
   --storage-access-key "${GCS_ACCESS_KEY}" --storage-secrets "${GCS_SECRET_KEY}" \
-  --storage-type "${storageType}" --storage-bucket "${streamBucket}"
+  --storage-type "${storageType}" --storage-bucket "${streamBucket}" $BUCKET_PREFIX_OPTION
+
+npm run solo-test -- explorer deploy -n "${SOLO_NAMESPACE}" -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
 
 kubectl port-forward -n "${SOLO_NAMESPACE}" svc/haproxy-node1-svc 50211:50211 > /dev/null 2>&1 &
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/hedera-explorer 8080:80 > /dev/null 2>&1 &
+
+explorer_svc="$(kubectl get svc -l app.kubernetes.io/component=hedera-explorer -n ${SOLO_NAMESPACE} --output json | jq -r '.items[].metadata.name')"
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/"${explorer_svc}" 8080:80 > /dev/null 2>&1 &
 
 cd ..; create_test_account ; cd -
 
@@ -74,10 +83,11 @@ node examples/create-topic.js
 
 npm run solo-test -- node stop -i node1 -n "${SOLO_NAMESPACE}"
 
+echo "Waiting for backup uploader to run"
 # manually call script "backup.sh" from container backup-uploader since it only runs every 5 minutes
-kubectl exec network-node1-0 -c backup-uploader -n solo-e2e -- /backup.sh > /dev/null 2>&1
+kubectl exec network-node1-0 -c backup-uploader -n solo-e2e -- /backup.sh
 
-# retrieve logs and check if it include the message type "error"
+echo "Retrieve logs and check if it include the error message"
 # example : {"level":"error","msg":"Updated modification time ......}
 kubectl logs network-node1-0 -c backup-uploader -n solo-e2e > backup-uploader.log
 if grep -q \""error\"" backup-uploader.log; then
