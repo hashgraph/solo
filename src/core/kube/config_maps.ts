071a055f025b19a25ef1fd747620ba6d929ac224
Jeromy Cannon <jeromy@swirldslabs.com>
2025-01-30 18:06:17 +0000

    added TSDoc to config_map.ts
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/core/kube/config_maps.ts
+++ src/core/kube/config_maps.ts
@@ -3,15 +3,42 @@
  */
 import {type V1ConfigMap} from '@kubernetes/client-node';
 
-export default interface ConfigMaps {
+export default interface Config_maps {
+  /**
+   * Create a new config map
+   * @param namespace - for the config map
+   * @param name - for the config name
+   * @param labels - for the config metadata
+   * @param data - to contain in the config
+   */
   create(
     namespace: string,
     name: string,
     labels: Record<string, string>,
     data: Record<string, string>,
   ): Promise<boolean>; // TODO was createNamespacedConfigMap
+
+  /**
+   * Delete a config map
+   * @param namespace - for the config map
+   * @param name - for the config name
+   */
   delete(namespace: string, name: string): Promise<boolean>; // TODO was deleteNamespacedConfigMap
+
+  /**
+   * Read a config map
+   * @param namespace - for the config map
+   * @param name - for the config name
+   */
   read(namespace: string, name: string): Promise<V1ConfigMap>; // TODO was getNamespacedConfigMap
+
+  /**
+   * Replace an existing config map with a new one
+   * @param namespace - for the config map
+   * @param name - for the config name
+   * @param labels - for the config metadata
+   * @param data - to contain in the config
+   */
   replace(
     namespace: string,
     name: string,
