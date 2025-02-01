73a147aa3f89bb6eded25b20f81d1a3f196fcc6b
Jeromy Cannon <jeromy@swirldslabs.com>
2025-02-01 17:07:36 +0000

    removed factory and interfaces for NamespaceName
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/core/kube/namespace_name.ts
+++ src/core/kube/namespace_name.ts
@@ -1,19 +1,31 @@
 /**
  * SPDX-License-Identifier: Apache-2.0
  */
+import {NamespaceNameInvalidError} from './kube_errors.js';
+import {isDns1123Label} from '../helpers.js';
+
 /**
  * Represents a Kubernetes namespace name. A Kubernetes namespace name must
  * be a valid DNS 1123 label.
  *
  * @include DNS_1123_LABEL
  */
-export interface NamespaceName {
+export class NamespaceName {
+  private constructor(public readonly name: string) {
+    if (!this.isValid()) {
+      throw new NamespaceNameInvalidError(NamespaceNameInvalidError.NAMESPACE_NAME_INVALID(name));
+    }
+  }
+
   /**
-   * The name of the namespace. A Kubernetes namespace name must be a valid DNS 1123 label.
+   * Creates a namespace. A Kubernetes namespace name must be a valid DNS 1123 label.
    *
    * @include DNS_1123_LABEL
+   * @param name The name of the namespace.
    */
-  readonly name: string;
+  public static of(name: string): NamespaceName {
+    return new NamespaceName(name);
+  }
 
   /**
    * Returns true if the namespace name is valid.  A Kubernetes namespace name must be a valid DNS 1123 label.
@@ -23,5 +35,7 @@ export interface NamespaceName {
    * @returns true if the namespace name is valid.
    * @throws NamespaceNameInvalidError if the namespace name is invalid.
    */
-  isValid(): boolean;
+  public isValid(): boolean {
+    return isDns1123Label(this.name);
+  }
 }
