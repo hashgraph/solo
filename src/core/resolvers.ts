9e3fae27e6f189c950f475568a64b8f85e579133
Ivo Yankov <ivo@devlabs.bg>
2025-02-03 16:35:32 +0200

    fix: failing build
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/core/resolvers.ts
+++ src/core/resolvers.ts
@@ -2,14 +2,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import {type LocalConfig} from './config/local_config.js';
-import {type IConfigManager} from '../types/managers.js';
 import {type ListrTaskWrapper} from 'listr2';
 import {type DeploymentName, type Namespace} from './config/remote/types.js';
+import {type ConfigManager} from './config_manager.js';
 import {Flags as flags} from '../commands/flags.js';
 
 export async function resolveNamespaceFromDeployment(
   localConfig: LocalConfig,
-  configManager: IConfigManager,
+  configManager: ConfigManager,
   task: ListrTaskWrapper<any, any, any>,
 ): Promise<Namespace> {
   await configManager.executePrompt(task, [flags.deployment]);
