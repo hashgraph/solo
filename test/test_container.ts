6c0a07888386ff4fdf417b129090b829c85f7d3d
Ivo Yankov <ivo@devlabs.bg>
2025-02-03 15:19:44 +0200

    chore: fix failing tests
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- test/test_container.ts
+++ test/test_container.ts
@@ -3,9 +3,12 @@
  */
 import path from 'path';
 import {Container} from '../src/core/container_init.js';
+import fs from 'fs';
 
 const cacheDirectory = path.join('test', 'data', 'tmp');
 
 export function resetTestContainer(cacheDir: string = cacheDirectory) {
+  const localConfigFile = 'local-config.yaml';
+  fs.copyFileSync(path.join('test', 'data', localConfigFile), path.join(cacheDirectory, localConfigFile));
   Container.getInstance().reset(cacheDir, 'debug', true);
 }
