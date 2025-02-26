21409c76e22cced0c918b5c7ee43e143c195bdb2
Jeromy Cannon <jeromy@swirldslabs.com>
2025-02-26 21:05:12 +0000

    main is async/await
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- solo.ts
+++ solo.ts
@@ -4,4 +4,14 @@
  */
 import * as fnm from './src/index.js';
 
-fnm.main(process.argv);
+fnm
+  .main(process.argv)
+  .then(() => {
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(1);
+  });
