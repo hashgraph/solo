7fc1b255f86f0ca1c91fe9402a3bbd18f467633b
Jeromy Cannon <jeromy@swirldslabs.com>
2025-02-26 21:02:12 +0000

    undo changes to match main for network.ts
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/commands/network.ts
+++ src/commands/network.ts
@@ -146,8 +146,6 @@ export class NetworkCommand extends BaseCommand {
       flags.debugNodeAlias,
       flags.loadBalancerEnabled,
       flags.log4j2Xml,
-      // TODO remove namespace once remote config gets it from local config
-      flags.namespace,
       flags.deployment,
       flags.nodeAliasesUnparsed,
       flags.persistentVolumeClaims,
@@ -611,8 +609,6 @@ export class NetworkCommand extends BaseCommand {
       flags.debugNodeAlias,
       flags.loadBalancerEnabled,
       flags.log4j2Xml,
-      // TODO remove namespace once remote config gets it from local config
-      flags.namespace,
       flags.persistentVolumeClaims,
       flags.profileName,
       flags.profileFile,
