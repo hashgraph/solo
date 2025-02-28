4def3083d8fb7c3f92c18d392bf9de347f15a4ea
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-02-28 09:37:20 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version.ts
+++ version.ts
@@ -7,7 +7,7 @@
  */
 
 export const HELM_VERSION = 'v3.14.2';
-export const SOLO_CHART_VERSION = '0.44.0';
+export const SOLO_CHART_VERSION = '0.45.0';
 export const HEDERA_PLATFORM_VERSION = 'v0.58.10';
 export const LOCAL_HEDERA_PLATFORM_VERSION = 'v0.58.3';
 export const MIRROR_NODE_VERSION = 'v0.122';
