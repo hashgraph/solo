0b2b7e7aede621782387ad81e088514b1f43f337
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-03 21:12:56 +0000

    fix: update Hedera platform version to v0.60.0-alpha.1
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- version.ts
+++ version.ts
@@ -8,8 +8,8 @@
 
 export const HELM_VERSION = 'v3.14.2';
 export const SOLO_CHART_VERSION = '0.44.0';
-export const HEDERA_PLATFORM_VERSION = 'v0.58.10';
-export const LOCAL_HEDERA_PLATFORM_VERSION = 'v0.58.3';
+export const HEDERA_PLATFORM_VERSION = 'v0.60.0-alpha.1';
+export const LOCAL_HEDERA_PLATFORM_VERSION = 'v0.58.10';
 export const MIRROR_NODE_VERSION = 'v0.122';
 export const HEDERA_EXPLORER_VERSION = '24.12.0';
 export const HEDERA_JSON_RPC_RELAY_VERSION = 'v0.63.2';
