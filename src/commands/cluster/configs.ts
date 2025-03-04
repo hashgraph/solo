e2de6a032e033f7db5ffc8533f80b89258c987e2
Ivo Yankov <ivo@devlabs.bg>
2025-03-04 14:48:10 +0200

    fix: incorrect brackets due to merging
    Signed-off-by: Ivo Yankov <ivoyankov@Ivos-MacBook-Pro.local>

--- src/commands/cluster/configs.ts
+++ src/commands/cluster/configs.ts
@@ -83,24 +83,24 @@ export class ClusterCommandConfigs {
       if (!confirmResult) {
         this.logger.logAndExitSuccess('Aborted application by user prompt');
       }
-
-      this.configManager.update(argv);
-
-      ctx.config = {
-        clusterName: this.configManager.getFlag(flags.clusterRef) as string,
-        clusterSetupNamespace: this.configManager.getFlag(flags.clusterSetupNamespace) as string,
-      } as ClusterResetConfigClass;
-
-      ctx.isChartInstalled = await this.chartManager.isChartInstalled(
-        ctx.config.clusterSetupNamespace,
-        constants.SOLO_CLUSTER_SETUP_CHART,
-      );
-      if (!ctx.isChartInstalled) {
-        throw new SoloError('No chart found for the cluster');
-      }
-
-      return ctx.config;
     }
+
+    this.configManager.update(argv);
+
+    ctx.config = {
+      clusterName: this.configManager.getFlag(flags.clusterRef) as string,
+      clusterSetupNamespace: this.configManager.getFlag(flags.clusterSetupNamespace) as string,
+    } as ClusterResetConfigClass;
+
+    ctx.isChartInstalled = await this.chartManager.isChartInstalled(
+      ctx.config.clusterSetupNamespace,
+      constants.SOLO_CLUSTER_SETUP_CHART,
+    );
+    if (!ctx.isChartInstalled) {
+      throw new SoloError('No chart found for the cluster');
+    }
+
+    return ctx.config;
   }
 }
 
