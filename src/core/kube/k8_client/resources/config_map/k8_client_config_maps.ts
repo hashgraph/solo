138ba212cd4ac9ae7c0a370e972ab88dec7b7a85
Ivo Yankov <ivo@devlabs.bg>
2025-03-10 12:16:36 +0200

    fix: revert change
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/core/kube/k8_client/resources/config_map/k8_client_config_maps.ts
+++ src/core/kube/k8_client/resources/config_map/k8_client_config_maps.ts
@@ -53,9 +53,7 @@ export class K8ClientConfigMaps implements ConfigMaps {
   }
 
   public async read(namespace: NamespaceName, name: string): Promise<V1ConfigMap> {
-    const {response, body} = await this.kubeClient.readNamespacedConfigMap(name, namespace.name).catch(e => {
-      throw new SoloError(`Error reading namespaced ConfigMap: ${e.message}`, e);
-    });
+    const {response, body} = await this.kubeClient.readNamespacedConfigMap(name, namespace.name).catch(e => e);
     KubeApiResponse.check(response, ResourceOperation.READ, ResourceType.CONFIG_MAP, namespace, name);
     return body as V1ConfigMap;
   }
