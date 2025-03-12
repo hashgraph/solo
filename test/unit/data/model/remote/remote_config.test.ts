955a24160f423093f5327616dc3f04e16d8424f0
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-12 21:31:49 +0000

    chore: rename remote-config.yaml to v0-34-0-remote-config.yaml
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/model/remote/remote_config.test.ts
+++ test/unit/data/model/remote/remote_config.test.ts
@@ -8,6 +8,15 @@ import os from 'os';
 import {RemoteConfig} from '../../../../../src/data/schema/model/remote/remote_config.js';
 import {instanceToPlain, plainToClass} from 'class-transformer';
 
+function migrateVersionPrefix(version: string): string {
+  const strippedVersionPrefix: string = version.replace(/^v/, '');
+  const parts = strippedVersionPrefix.split('.').map(Number); // Split and convert to numbers
+  while (parts.length < 3) {
+    parts.push(0); // Add missing minor/patch as 0
+  }
+  return parts.join('.');
+}
+
 function migrate(plainObject: object): void {
   plainObject['schemaVersion'] = 0;
   const meta: object = plainObject['metadata'];
@@ -17,23 +26,43 @@ function migrate(plainObject: object): void {
   };
 
   const clusters: object = plainObject['clusters'];
-  const ca: object[] = [];
+  const clustersArray: object[] = [];
   for (const key in clusters) {
     expect(clusters[key]).to.not.be.undefined.and.to.not.be.null;
-    const c = clusters[key];
-    ca.push(c);
+    const cluster = clusters[key];
+    clustersArray.push(cluster);
   }
+
+  plainObject['versions'] = {};
+  plainObject['versions']['cli'] = migrateVersionPrefix(plainObject['metadata']?.['soloVersion'] || '0.0.0');
+  plainObject['versions']['chart'] = migrateVersionPrefix(plainObject['metadata']?.['soloChartVersion'] || '0.0.0');
+  plainObject['versions']['consensusNode'] = migrateVersionPrefix(
+    plainObject['metadata']?.['hederaPlatformVersion'] || plainObject['flags']?.['releaseTag'] || '0.0.0',
+  );
+  plainObject['versions']['mirrorNodeChart'] = migrateVersionPrefix(
+    plainObject['metadata']?.['hederaMirrorNodeChartVersion'] || plainObject['flags']?.['mirrorNodeVersion'] || '0.0.0',
+  );
+  plainObject['versions']['explorerChart'] = migrateVersionPrefix(
+    plainObject['metadata']?.['hederaExplorerChartVersion'] ||
+      plainObject['flags']?.['hederaExplorerVersion'] ||
+      '0.0.0',
+  );
+
+  plainObject['versions']['jsonRpcRelayChart'] = migrateVersionPrefix(
+    plainObject['metadata']?.['hederaJsonRpcRelayChartVersion'] || plainObject['flags']?.['relayReleaseTag'] || '0.0.0',
+  );
+
   plainObject['history'] = {};
   plainObject['history']['commands'] = [];
   for (const historyItem of plainObject['commandHistory']) {
     plainObject['history']['commands'].push(historyItem);
   }
 
-  plainObject['clusters'] = ca;
+  plainObject['clusters'] = clustersArray;
 }
 
 describe('RemoteConfig', () => {
-  const remoteConfigPath = 'test/data/remote-config.yaml';
+  const remoteConfigPath = 'test/data/v0-34-0-remote-config.yaml';
 
   describe('Class Transformer', () => {
     let yamlData: string;
@@ -50,9 +79,15 @@ describe('RemoteConfig', () => {
     });
 
     it('should transform plain to class', async () => {
-      const rc = plainToClass(RemoteConfig, plainObject);
+      const rc: RemoteConfig = plainToClass(RemoteConfig, plainObject);
       expect(rc).to.not.be.undefined.and.to.not.be.null;
       expect(rc.history.commands.length).to.be.equal(1);
+      expect(rc.versions.cli.version).to.equal('0.34.0');
+      expect(rc.versions.chart.version).to.equal('0.0.0');
+      expect(rc.versions.consensusNode.version).to.equal('0.58.10');
+      expect(rc.versions.mirrorNodeChart.version).to.equal('0.122.0');
+      expect(rc.versions.explorerChart.version).to.equal('24.12.0');
+      expect(rc.versions.jsonRpcRelayChart.version).to.equal('0.63.2');
     });
 
     it('should transform class to plain', async () => {
