664f60994a866c331f0d1500194e06a4d34b3802
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-12 21:02:40 +0000

    test: enhance remote config tests to validate history and transformation
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/model/remote/remote_config.test.ts
+++ test/unit/data/model/remote/remote_config.test.ts
@@ -6,7 +6,7 @@ import {expect} from 'chai';
 import {beforeEach} from 'mocha';
 import os from 'os';
 import {RemoteConfig} from '../../../../../src/data/schema/model/remote/remote_config.js';
-import {plainToClass} from 'class-transformer';
+import {instanceToPlain, plainToClass} from 'class-transformer';
 
 function migrate(plainObject: object): void {
   plainObject['schemaVersion'] = 0;
@@ -22,6 +22,11 @@ function migrate(plainObject: object): void {
     expect(clusters[key]).to.not.be.undefined.and.to.not.be.null;
     const c = clusters[key];
     ca.push(c);
+  }
+  plainObject['history'] = {};
+  plainObject['history']['commands'] = [];
+  for (const historyItem of plainObject['commandHistory']) {
+    plainObject['history']['commands'].push(historyItem);
   }
 
   plainObject['clusters'] = ca;
@@ -47,8 +52,13 @@ describe('RemoteConfig', () => {
     it('should transform plain to class', async () => {
       const rc = plainToClass(RemoteConfig, plainObject);
       expect(rc).to.not.be.undefined.and.to.not.be.null;
+      expect(rc.history.commands.length).to.be.equal(1);
     });
 
-    it('should transform class to plain', async () => {});
+    it('should transform class to plain', async () => {
+      const rc: RemoteConfig = plainToClass(RemoteConfig, plainObject);
+      const remoteConfigObject = instanceToPlain(rc);
+      expect(remoteConfigObject).to.not.be.undefined.and.to.not.be.null;
+    });
   });
 });
