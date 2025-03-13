e766d9dbba753dfc33056071d0c06433eead0dcb
Nathan Klick <nathan@swirldslabs.com>
2025-03-13 13:05:39 -0500

    fix: resolve import errors
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- src/data/configuration/api/configuration_error.ts
+++ src/data/configuration/api/configuration_error.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {SoloError} from '../../../core/errors.js';
+import {SoloError} from '../../../core/errors/SoloError.js';
 
 /**
  * General purpose error for configuration failures.
