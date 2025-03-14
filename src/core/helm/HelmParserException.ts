ba88607439b141ffd95db6fa64958f277d3ff7ca
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-03-14 09:04:41 -0500

    Save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/core/helm/HelmParserException.ts
+++ src/core/helm/HelmParserException.ts
@@ -4,23 +4,6 @@
  * An exception thrown when parsing the output of a helm command fails.
  */
 export class HelmParserException extends Error {
-  /**
-   * Constructs a new runtime exception with the specified detail message.
-   * @param message The detail message
-   */
-  constructor(message: string);
-  /**
-   * Constructs a new runtime exception with the specified detail message and cause.
-   * @param message The detail message
-   * @param cause The cause
-   */
-  constructor(message: string, cause: Error);
-  /**
-   * Constructs a new runtime exception with the specified cause.
-   * @param cause The cause
-   */
-  constructor(cause: Error);
-
   constructor(messageOrCause: string | Error, cause?: Error) {
     if (messageOrCause instanceof Error) {
       super(messageOrCause.message);
@@ -33,6 +16,4 @@ export class HelmParserException extends Error {
     }
     this.name = 'HelmParserException';
   }
-
-  cause?: Error;
 }
