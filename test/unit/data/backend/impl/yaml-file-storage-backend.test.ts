647ead67236022b72950003e878e3ef8f6efa42c
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-14 17:07:19 +0000

    test: add readObject test for handling empty files in YAML File Storage Backend
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/backend/impl/yaml-file-storage-backend.test.ts
+++ test/unit/data/backend/impl/yaml-file-storage-backend.test.ts
@@ -3,17 +3,27 @@
 import {getTmpDir} from '../../../../test-util.js';
 import {YamlFileStorageBackend} from '../../../../../src/data/backend/impl/yaml-file-storage-backend.js';
 import {expect} from 'chai';
+import path from 'path';
+import fs from 'fs';
 
 describe('YAML File Storage Backend', () => {
   const testName: string = 'yaml-file-storage-backend';
   const tempDir: string = getTmpDir();
 
-  it('test writeObject', async () => {
+  it('test readObject and writeObject', async () => {
     const key: string = `${testName}-file.yaml`;
     const backend: YamlFileStorageBackend = new YamlFileStorageBackend(tempDir);
     await backend.writeObject(key, {key: 'value'});
     const data: object = await backend.readObject(key);
     expect(data).to.be.an('object');
     expect(data).to.deep.equal({key: 'value'});
+  });
+
+  it('test readObject with empty file', async () => {
+    const key: string = `${testName}-file2.yaml`;
+    const temporaryFile: string = path.join(tempDir, key);
+    const backend: YamlFileStorageBackend = new YamlFileStorageBackend(tempDir);
+    fs.writeFileSync(temporaryFile, '');
+    await expect(backend.readObject(key)).to.be.rejectedWith('file is empty');
   });
 });
