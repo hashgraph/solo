3a348ff5975ecaa17097ae5795c2e4713e727689
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-03-18 20:33:04 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/node/handlers.ts
+++ src/commands/node/handlers.ts
@@ -88,6 +88,8 @@ export class NodeCommandHandlers extends CommandHandler {
       this.tasks.copyNodeKeysToSecrets(),
       this.tasks.getNodeLogsAndConfigs(),
       this.tasks.updateChartWithConfigMap('Delete network node', NodeSubcommandType.DELETE),
+      this.tasks.killNodes(),
+      this.tasks.sleep('Give time for pods to come up after being killed', 20000),
       this.tasks.checkNodePodsAreRunning(),
       this.tasks.populateServiceMap(),
       this.tasks.fetchPlatformSoftware('allNodeAliases'),
