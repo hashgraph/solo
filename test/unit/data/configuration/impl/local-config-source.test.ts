bcf2eae75547dca4c85f2c2dacd67b73c43914aa
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-21 21:20:21 +0000

    refactor: improve asBoolean method logic and add unit tests for LocalConfigSource
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/configuration/impl/local-config-source.test.ts
+++ test/unit/data/configuration/impl/local-config-source.test.ts
@@ -2,11 +2,31 @@
 
 import {expect} from 'chai';
 import {LocalConfigSource} from '../../../../../src/data/configuration/impl/local-config-source.js';
+import {LocalConfigSchema} from '../../../../../src/data/schema/migration/impl/local/local-config-schema.js';
+import {type ObjectMapper} from '../../../../../src/data/mapper/api/object-mapper.js';
+import {CTObjectMapper} from '../../../../../src/data/mapper/impl/ct-object-mapper.js';
+import {ConfigKeyFormatter} from '../../../../../src/data/key/config-key-formatter.js';
+import {SimpleObjectStorageBackend} from '../../../fixtures/simple-object-storage-backend.fixture.js';
 
 describe('LocalConfigSource', () => {
   it('constructor with null ObjectMapper throws error', () => {
     expect(() => {
       new LocalConfigSource(null, null, null, null);
     }).to.throw('ObjectMapper is required');
+  });
+
+  it('asBoolean with null value returns null', async () => {
+    const objectMapper: ObjectMapper = new CTObjectMapper(ConfigKeyFormatter.instance());
+    const map: Map<string, object> = new Map<string, object>();
+    map.set('key', {local: {key: 'fred'}});
+    const source: LocalConfigSource = new LocalConfigSource(
+      'key',
+      new LocalConfigSchema(objectMapper),
+      objectMapper,
+      new SimpleObjectStorageBackend(map),
+    );
+    await source.load();
+    expect(source.asBoolean('useridentity')).to.be.null;
+    expect(source.asBoolean('useridentity.hostname')).to.be.false;
   });
 });
---
+++ test/unit/fixtures/simple-object-storage-backend.fixture.ts
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import {type ObjectStorageBackend} from '../../../src/data/backend/api/object-storage-backend.js';
+import {type StorageOperation} from '../../../src/data/backend/api/storage-operation.js';
+
+export class SimpleObjectStorageBackend implements ObjectStorageBackend {
+  public constructor(public map: Map<string, object>) {
+    if (!map) {
+      throw new Error('Map is required');
+    }
+  }
+
+  public readObject(key: string): Promise<object> {
+    return Promise.resolve(this.map.get(key));
+  }
+
+  public writeObject(key: string, data: object): Promise<void> {
+    this.map.set(key, data);
+    return Promise.resolve();
+  }
+
+  public list(): Promise<string[]> {
+    return Promise.resolve(Array.from(this.map.keys()));
+  }
+
+  public readBytes(key: string): Promise<Buffer> {
+    return Promise.resolve(Buffer.from(JSON.stringify(this.map.get(key))));
+  }
+
+  public writeBytes(key: string, data: Buffer): Promise<void> {
+    this.map.set(key, JSON.parse(data.toString()));
+    return Promise.resolve();
+  }
+
+  public delete(key: string): Promise<void> {
+    this.map.delete(key);
+    return Promise.resolve();
+  }
+
+  public isSupported(op: StorageOperation): boolean {
+    return true;
+  }
+}
