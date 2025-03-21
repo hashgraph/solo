860c9e660f6297700a25735cde89701286202e31
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-21 19:18:00 +0000

    refactor: remove unused EnvironmentStorageBackend and add YamlFileStorageBackend in tests
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/configuration/impl/layered-model-config-source.test.ts
+++ test/unit/data/configuration/impl/layered-model-config-source.test.ts
@@ -2,6 +2,7 @@
 
 import {expect} from 'chai';
 import {LayeredModelConfigSource} from '../../../../../src/data/configuration/impl/layered-model-config-source.js';
+import {YamlFileStorageBackend} from '../../../../../src/data/backend/impl/yaml-file-storage-backend.js';
 
 describe('LayeredModelConfigSource', () => {
   it('constructor with null ObjectMapper throws error', () => {
@@ -9,5 +10,12 @@ describe('LayeredModelConfigSource', () => {
       // @ts-expect-error - testing protected constructor
       new LayeredModelConfigSource(null, null, null, null);
     }).to.throw('ObjectMapper is required');
+  });
+
+  it('test modelData getter and setter', () => {
+    // @ts-expect-error - testing protected constructor
+    const source = new LayeredModelConfigSource('key', {}, new YamlFileStorageBackend('.'), {});
+    source.modelData = 'modelData';
+    expect(source.modelData).to.equal('modelData');
   });
 });
