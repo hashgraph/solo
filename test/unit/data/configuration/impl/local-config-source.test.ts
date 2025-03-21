f86255ea5fa0dfd92f8ebee68c6aa4e04c7ae2d4
Jeromy Cannon <jeromy@swirldslabs.com>
2025-03-21 19:03:46 +0000

    refactor: update import paths in test files for improved readability
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/data/configuration/impl/local-config-source.test.ts
+++ test/unit/data/configuration/impl/local-config-source.test.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {expect} from 'chai';
-import {LocalConfigSource} from '../../../../../../src/data/configuration/impl/local-config-source.js';
+import {LocalConfigSource} from '../../../../../src/data/configuration/impl/local-config-source.js';
 
 describe('LocalConfigSource', () => {
   it('constructor with null ObjectMapper throws error', () => {
