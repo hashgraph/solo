1911b4678e0daf56205e7b68a426dc63232a5f2b
Ivo Yankov <ivo@devlabs.bg>
2025-03-24 11:23:26 +0200

    chore: refactor render method
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/core/help-renderer.ts
+++ src/core/help-renderer.ts
@@ -5,9 +5,11 @@ import {InjectTokens} from './dependency-injection/inject-tokens.js';
 import {type SoloLogger} from './logging.js';
 import {patchInject} from './dependency-injection/container-helper.js';
 
+type Table = string[][];
+
 @injectable()
 export class HelpRenderer {
-  constructor(@inject(InjectTokens.SoloLogger) private readonly logger: SoloLogger) {
+  public constructor(@inject(InjectTokens.SoloLogger) private readonly logger: SoloLogger) {
     this.logger = patchInject(logger, InjectTokens.SoloLogger, this.constructor.name);
   }
 
@@ -16,7 +18,7 @@ export class HelpRenderer {
       return [input, ''];
     }
 
-    const splitIndex = input.lastIndexOf(' ', maxLength);
+    const splitIndex: number = input.lastIndexOf(' ', maxLength);
     if (splitIndex === -1) {
       return [input, ''];
     }
@@ -24,66 +26,56 @@ export class HelpRenderer {
     return [input.substring(0, splitIndex), input.substring(splitIndex + 1)];
   }
 
-  public render(rootCmd: any, rawHelp: string) {
-    const splittingString = 'Options:\n';
-    const splitOutput = rawHelp.split(splittingString);
-    if (splitOutput.length < 2) {
-      this.logger.showUser(rawHelp);
-      return;
-    }
-
-    let finalOutput = splitOutput[0] + splittingString;
-    let lines = splitOutput[1].split('\n');
-    lines = lines.map(line => line.replace(/^\s+/, ''));
-
-    //Formatting for flag options
-    const table = [];
+  private createFlagsTable(lines: string[]): Table {
+    const table: Table = [];
     for (const line of lines) {
-      let columns = line.split(/(--[1-9a-zA-Z|-]+)/);
+      let columns: string[] = line.split(/(--[1-9a-zA-Z|-]+)/);
 
-      // if the description contains --flag there will be more that the expected amount of columns
+      // if the description contains --flag there will be more than the expected amount of columns
       // joins all columns after columns[2]
       if (columns.length > 3) {
-        const firstPart = columns.slice(0, 2);
-        const secondPart = columns.slice(2).join(' ');
+        const firstPart: string[] = columns.slice(0, 2);
+        const secondPart: string = columns.slice(2).join(' ');
         columns = [...firstPart, secondPart];
       }
 
-      const desciptors = columns[2].split(/(\[.+])/);
-      columns[2] = desciptors[0];
-      columns[3] = desciptors[1] || '';
-      columns = columns.map(column => column.trim());
+      const descriptions: string[] = columns[2].split(/(\[.+])/);
+      columns[2] = descriptions[0];
+      columns[3] = descriptions[1] || '';
+      columns = columns.map((column: string): string => column.trim());
       table.push(columns);
     }
 
-    // apply sorting
-    table.sort((row1, row2) => {
+    return table;
+  }
+
+  private sortFlagsTable(table: Table): Table {
+    table.sort((row1: string[], row2: string[]) => {
       return row1[1].localeCompare(row2[1]);
     });
 
-    const requiredTable = table.filter(row => row[3].includes('required'));
-    const optionalTable = table.filter(row => !row[3].includes('required'));
-    const sortedTable = requiredTable.concat(optionalTable);
+    const requiredTable: Table = table.filter((row: string[]): boolean => row[3].includes('required'));
+    const optionalTable: Table = table.filter((row: string[]): boolean => !row[3].includes('required'));
+    return requiredTable.concat(optionalTable);
+  }
 
-    const columnMaxLengths = [0, 0, 0, 0];
-    for (const row of sortedTable) {
+  private calculateMaxColumnLengths(table: Table): number[] {
+    const columnMaxLengths: number[] = [0, 0, 0, 0];
+    for (const row of table) {
       for (let i = 0; i < row.length; i++) {
         columnMaxLengths[i] = Math.max(columnMaxLengths[i], row[i].length);
       }
     }
 
-    // wrap the description column at the wrapping point
-    const terminalWidth = rootCmd.terminalWidth();
-    let wrap = terminalWidth - columnMaxLengths[0] - columnMaxLengths[1] - columnMaxLengths[3] - 6;
-    if (wrap < 30) wrap = 30; // set min and max values
-    if (wrap > 70) wrap = 70;
-    if (columnMaxLengths[2] < wrap) wrap = columnMaxLengths[2];
-    else columnMaxLengths[2] = wrap;
-    const wrappedTable = [];
-    for (const row of sortedTable) {
+    return columnMaxLengths;
+  }
+
+  private wrapFlagsTable(table: Table, columnMaxLengths: number[], wrap: number): Table {
+    const wrappedTable: Table = [];
+    for (const row of table) {
       if (row[2].length > wrap) {
-        const description = row[2];
-        let splitDescription = this.splitAtClosestWhitespace(description, wrap);
+        const description: string = row[2];
+        let splitDescription: [string, string] = this.splitAtClosestWhitespace(description, wrap);
         wrappedTable.push([row[0], row[1], splitDescription[0], row[3]]);
         while (splitDescription[1] && splitDescription[1].length > 0) {
           splitDescription = this.splitAtClosestWhitespace(splitDescription[1], wrap);
@@ -94,14 +86,57 @@ export class HelpRenderer {
       }
     }
 
-    const outputLines = [];
-    for (const row of wrappedTable) {
-      const line = [];
-      for (let i = 0; i < row.length; i++) {
+    return wrappedTable;
+  }
+
+  private getDescriptionWrap(terminalWidth: number, columnMaxLengths: number[]): number {
+    let wrap: number = terminalWidth - columnMaxLengths[0] - columnMaxLengths[1] - columnMaxLengths[3] - 6;
+    if (wrap < 30) wrap = 30; // set min and max values
+    if (wrap > 70) wrap = 70;
+    if (columnMaxLengths[2] < wrap) wrap = columnMaxLengths[2];
+    else columnMaxLengths[2] = wrap;
+    return wrap;
+  }
+
+  private addColumnPadding(table: Table, columnMaxLengths: number[]): string[] {
+    const outputLines: string[] = [];
+    for (const row of table) {
+      const line: string[] = [];
+      for (let i: number = 0; i < row.length; i++) {
         line.push(row[i].padEnd(columnMaxLengths[i]));
       }
       outputLines.push(line.join('  '));
     }
+
+    return outputLines;
+  }
+
+  public render(rootCmd: any, rawHelp: string): void {
+    const splittingString: string = 'Options:\n';
+    const splitOutput: string[] = rawHelp.split(splittingString);
+    if (splitOutput.length < 2) {
+      this.logger.showUser(rawHelp);
+      return;
+    }
+
+    let finalOutput: string = splitOutput[0] + splittingString;
+    let lines: string[] = splitOutput[1].split('\n');
+    lines = lines.map((line: string): string => line.replace(/^\s+/, ''));
+
+    // Formatting for flag options
+    const table: Table = this.createFlagsTable(lines);
+
+    // apply sorting
+    const sortedTable: Table = this.sortFlagsTable(table);
+
+    const columnMaxLengths: number[] = this.calculateMaxColumnLengths(sortedTable);
+
+    // wrap the description column at the wrapping point
+    const terminalWidth: number = rootCmd.terminalWidth();
+    const wrap: number = this.getDescriptionWrap(terminalWidth, columnMaxLengths);
+
+    const wrappedTable = this.wrapFlagsTable(sortedTable, columnMaxLengths, wrap);
+    const outputLines = this.addColumnPadding(wrappedTable, columnMaxLengths);
 
     finalOutput += '\n';
     finalOutput += outputLines.join('\n');
