916bd77bc446d391922912086981c057239f29d3
Jeromy Cannon <jeromy@swirldslabs.com>
2025-04-14 19:25:29 +0100

    refactor: update chart URLs and version numbers for mirror node and explorer
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -61,17 +61,17 @@ export const SOLO_DEPLOYMENT_CHART = 'solo-deployment';
 export const SOLO_CERT_MANAGER_CHART = 'solo-cert-manager';
 
 export const JSON_RPC_RELAY_CHART_URL =
-  process.env.JSON_RPC_RELAY_CHART_URL ?? 'https://hashgraph.github.io/hedera-json-rpc-relay/charts';
+  process.env.JSON_RPC_RELAY_CHART_URL ?? 'https://hiero-ledger.github.io/hiero-json-rpc-relay/charts';
 export const JSON_RPC_RELAY_CHART = 'hedera-json-rpc-relay';
 
 export const MIRROR_NODE_CHART_URL =
-  process.env.MIRROR_NODE_CHART_URL ?? 'https://hashgraph.github.io/hedera-mirror-node/charts';
+  process.env.MIRROR_NODE_CHART_URL ?? 'https://hiero-ledger.github.io/hiero-mirror-node/charts';
 export const MIRROR_NODE_CHART = 'hedera-mirror';
 export const MIRROR_NODE_RELEASE_NAME = 'mirror';
 
 export const HEDERA_EXPLORER_CHART_URL =
-  process.env.HEDERA_EXPLORER_CHART_URL ?? 'oci://ghcr.io/hashgraph/hedera-mirror-node-explorer/hedera-explorer-chart';
-export const HEDERA_EXPLORER_RELEASE_NAME = 'hedera-explorer';
+  process.env.HEDERA_EXPLORER_CHART_URL ?? 'oci://ghcr.io/hiero-ledger/hiero-mirror-node-explorer/hiero-explorer-chart';
+export const HEDERA_EXPLORER_RELEASE_NAME = 'hiero-explorer';
 export const SOLO_RELAY_LABEL = 'app=hedera-json-rpc-relay';
 export const SOLO_HEDERA_EXPLORER_LABEL = 'app.kubernetes.io/component=hedera-explorer';
 
