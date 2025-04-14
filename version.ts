e7900160116607d4057e88821c01f59212d07ee0
Ivo Yankov <ivo@devlabs.bg>
2025-04-14 18:02:12 +0300

    chore: bump consensus node to v61.2
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- version.ts
+++ version.ts
@@ -12,7 +12,7 @@ import fs from 'node:fs';
 
 export const HELM_VERSION = 'v3.14.2';
 export const SOLO_CHART_VERSION = '0.50.0';
-export const HEDERA_PLATFORM_VERSION = 'v0.59.5';
+export const HEDERA_PLATFORM_VERSION = 'v0.61.2';
 export const MIRROR_NODE_VERSION = 'v0.126.0';
 export const HEDERA_EXPLORER_VERSION = '24.12.1';
 export const HEDERA_JSON_RPC_RELAY_VERSION = 'v0.66.0';
