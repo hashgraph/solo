72414091d17bd238ed76e58fd49f0299322359f1
Jeromy Cannon <jeromy@swirldslabs.com>
2025-04-15 19:07:39 +0100

    made fixes recommended by Matt Hess to address issues in consensus node when reading genesis-network.json
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/unit/core/helpers.test.ts
+++ test/unit/core/helpers.test.ts
@@ -7,12 +7,12 @@ import {Flags as flags} from '../../../src/commands/flags.js';
 
 import * as helpers from '../../../src/core/helpers.js';
 
-describe('Helpers', () => {
+describe('Helpers', (): void => {
   each([
     {input: '', output: []},
     {input: 'node1', output: ['node1']},
     {input: 'node1,node3', output: ['node1', 'node3']},
-  ]).it('should parse node aliases for input', ({input, output}: {input: string; output: string[]}) => {
+  ]).it('should parse node aliases for input', ({input, output}: {input: string; output: string[]}): void => {
     expect(helpers.parseNodeAliases(input)).to.deep.equal(output);
   });
 
@@ -20,27 +20,27 @@ describe('Helpers', () => {
     {input: [], output: []},
     {input: [1, 2, 3], output: [1, 2, 3]},
     {input: ['a', '2', '3'], output: ['a', '2', '3']},
-  ]).it('should clone array for input', ({input, output}: {input: number[]; output: number[]}) => {
-    const clonedArray = helpers.cloneArray(input);
+  ]).it('should clone array for input', ({input, output}: {input: number[]; output: number[]}): void => {
+    const clonedArray: number[] = helpers.cloneArray(input);
     expect(clonedArray).to.deep.equal(output);
     expect(clonedArray).not.to.equal(input); // ensure cloning creates a new array
   });
 
-  it('Should parse argv to args with boolean flag correctly', () => {
-    const argv = {[flags.quiet.name]: true};
-    const result = flags.stringifyArgv(argv);
+  it('Should parse argv to args with boolean flag correctly', (): void => {
+    const argv: {[p: string]: boolean} = {[flags.quiet.name]: true};
+    const result: string = flags.stringifyArgv(argv);
     expect(result).to.equal(`--${flags.quiet.name}`);
   });
 
-  it('Should parse argv to args with flag correctly', () => {
-    const argv = {[flags.namespace.name]: 'VALUE'};
-    const result = flags.stringifyArgv(argv);
+  it('Should parse argv to args with flag correctly', (): void => {
+    const argv: {[p: string]: string} = {[flags.namespace.name]: 'VALUE'};
+    const result: string = flags.stringifyArgv(argv);
     expect(result).to.equal(`--${flags.namespace.name} VALUE`);
   });
 
-  it('Should ipv4ToByteArray convert IPv4 address to byte array', () => {
+  it('Should ipv4ToByteArray convert IPv4 address to string', (): void => {
     const ipV4Address: string = '192.168.0.1';
-    const byteArray: number[] = helpers.ipv4ToByteArray(ipV4Address);
-    expect(JSON.stringify(byteArray)).to.equal('[192,168,0,1]');
+    const byteString: string = helpers.ipv4ToBase64(ipV4Address);
+    expect(byteString).to.equal('wKgAAQ==');
   });
 });
